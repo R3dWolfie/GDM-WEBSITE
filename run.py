10c90b604cf8658df5562bc7f61a561a5564e2da
RedWolfie <arui939@gmail.com>
2023-07-15 21:58:28 +0300

    :white_check_mark: Added patreon checking

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import requests
+import patreon
 from flask import Flask, render_template
-from patreon import API, OAuth
 
 app = Flask(__name__, template_folder='templates')
 
@@ -8,13 +8,23 @@ client_id = "tWe8WLdrzldJNM1W2wwQO47x6w3V-jXKWHxqoKpAEQYkstEXQHJndxUN01qvSw2n"  
 client_secret = "nddXb3R832dFtzEo62RBwRX6BIXK86NpeE5dXGrbbTAsUPCEvZYy5A3E8yz8BKSa"  # Replace with your client secret
 access_token = "eWl9Ls5o4sebZvXfkmRFRUL-qcSbiHZEgIAbyxPULAA"  # Replace with your access token
 
-oauth_client = OAuth(client_id, client_secret)
-api_client = API(access_token)
+api_client = patreon.API(access_token)
 
-def get_latest_supporters():
-    latest_supporters = ["Reddie", "Lamnhiem", "Sirrilist"]
-    return latest_supporters
+# Get the campaign ID
+campaign_response = api_client.fetch_campaign()
+campaign_id = campaign_response.data()[0].id()
 
+
+all_pledges = []
+
+cursor = None
+while True:
+    pledges_response = api_client.fetch_page_of_pledges(campaign_id, 25, cursor=cursor)
+
+    cursor = api_client.extract_cursor(pledges_response)
+    all_pledges += pledges_response.data()
+    if not cursor:
+        break
 
 
 
@@ -23,24 +33,30 @@ def get_latest_supporters():
 def home():
     buy_vip_url = '/buy-vip'
     discord_url = 'https://discord.gg/gdm'
-    latest_supporters = get_latest_supporters()
-    return render_template('home.html', buy_vip_url=buy_vip_url, discord_url=discord_url,
-                           latest_supporters=latest_supporters)
+    latest_supporters = []
+
+    for pledge in all_pledges:
+        latest_supporters.append(pledge.relationship('patron').attribute('full_name'))
+
+    # latest_supporters = get_latest_supporters()
+    return render_template('home.html', buy_vip_url=buy_vip_url, discord_url=discord_url, latest_supporters=latest_supporters)
 
 
 @app.route('/downloads')
 def downloads():
     return render_template('downloads.html')
 
+
 @app.route('/VIP')
 def VIP():
     return render_template('VIP.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html.')
 
+
 if __name__ == '__main__':
-    get_latest_supporters()
-    print(get_latest_supporters())
+    print("Fetching patrons...")
     app.run(debug=True, host="0.0.0.0")
